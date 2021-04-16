0582f20fce7744a303db0d5103103dc3a7abd918
Alice SEBEGO <alice.etvotrecom@gmail.com>
2021-04-16 23:37:06 +0200

    Create taskDone function

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,7 @@ const $info = document.querySelector("#info");
 const $recap = document.querySelector("#recap");
 
 const $ul = document.querySelector("ul");
-const $done = document.querySelector("#done");
+const $done = document.querySelector(".done");
 const $deleteTask = document.querySelector("#deleteTask");
 
 // Array to fill with tasks
@@ -21,7 +21,7 @@ $form.addEventListener("submit", e =>{
 
     if(task.length > 0){
 
-        addTask(task);
+        createTask(task);
         $form.reset();
 
     } else {
@@ -33,14 +33,33 @@ $form.addEventListener("submit", e =>{
 
 });
 
-const addTask = (task) =>{
+const createTask = (task) =>{
+
+    const todo = {
+        task,
+        id: Date.now()
+    }
+
+    addTask(todo);
+
+}
+
+const addTask = (todo) =>{
     const $li = document.createElement("li");
     $li.setAttribute("tabindex", "0");
+    $li.setAttribute("data-key", todo.id);
     $li.innerHTML = `
-    <input type="checkbox" name="done" id="done" tabindex="0">
-    <label for="done" tabindex="0"></label> 
-    <span class="description">${task}</span>
+    <input type="checkbox" name="done" class="done" tabindex="0">
+    <label for="done" tabindex="0" class="labelDone"></label> 
+    <span class="description">${todo.task}</span>
     <button class="deleteTask"><i class="fas fa-times"></i></button> 
     `;
+    $done.addEventListener("click", taskDone);
+    // $deleteTask.addEventListener("click", deleteTask);
     $ul.appendChild($li);
+    taskList.push($li);
+}
+
+const taskDone = (e)=>{
+    e.target.parentNode.classList.toggle("taskDone");
 }
